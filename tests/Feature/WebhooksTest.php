5c8a493d93660093cf36492ae0cabde6a8565452
Stefan Zweifel <hello@stefanzweifel.io>
2020-07-06 20:41:33 +0200

    Add more Webhook Tests

--- tests/Feature/WebhooksTest.php
+++ tests/Feature/WebhooksTest.php
@@ -32,9 +32,80 @@ class WebhooksTest extends FeatureTestCase
         ]);
     }
 
+    /** @test */
+    public function test_it_can_handle_a_payment_succeeded_event_when_billable_already_exists()
+    {
+        if (! isset($_SERVER['PADDLE_TEST_CHECKOUT'])) {
+            $this->markTestSkipped('Checkout identifier not configured');
+        }
+
+        $user = $this->createUser();
+        $user->customer()->create([
+            'trial_ends_at' => now('UTC')->addDays(5)
+        ]);
+
+        $this->postJson('paddle/webhook', [
+            'alert_name' => 'payment_succeeded',
+            'checkout_id' => $_SERVER['PADDLE_TEST_CHECKOUT'],
+            'email' => $user->paddleEmail(),
+            'passthrough' => json_encode([
+                'billable_id' => $user->id,
+                'billable_type' => $user->getMorphClass(),
+            ]),
+        ])->assertOk();
+
+        $this->assertDatabaseHas('customers', [
+            'billable_id' => $user->id,
+            'billable_type' => $user->getMorphClass(),
+            'paddle_email' => $user->paddleEmail(),
+        ]);
+    }
+
     public function test_it_can_handle_a_subscription_created_event()
     {
         $user = $this->createUser();
+
+        $this->postJson('paddle/webhook', [
+            'alert_name' => 'subscription_created',
+            'user_id' => 'foo',
+            'email' => $user->paddleEmail(),
+            'passthrough' => json_encode([
+                'billable_id' => $user->id,
+                'billable_type' => $user->getMorphClass(),
+                'subscription_name' => 'main',
+            ]),
+            'quantity' => 1,
+            'status' => Subscription::STATUS_ACTIVE,
+            'subscription_id' => 'bar',
+            'subscription_plan_id' => 1234,
+        ])->assertOk();
+
+        $this->assertDatabaseHas('customers', [
+            'billable_id' => $user->id,
+            'billable_type' => $user->getMorphClass(),
+            'paddle_id' => 'foo',
+            'paddle_email' => $user->paddleEmail(),
+        ]);
+
+        $this->assertDatabaseHas('subscriptions', [
+            'billable_id' => $user->id,
+            'billable_type' => $user->getMorphClass(),
+            'name' => 'main',
+            'paddle_id' => 'bar',
+            'paddle_plan' => 1234,
+            'paddle_status' => Subscription::STATUS_ACTIVE,
+            'quantity' => 1,
+            'trial_ends_at' => null,
+        ]);
+    }
+
+    /** @test */
+    public function test_it_can_handle_a_subscription_created_event_if_billable_already_exists()
+    {
+        $user = $this->createUser();
+        $user->customer()->create([
+            'trial_ends_at' => now('UTC')->addDays(5)
+        ]);
 
         $this->postJson('paddle/webhook', [
             'alert_name' => 'subscription_created',
