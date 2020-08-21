cc6404809110673aa4275a91365a3dac007ddb7f
Rias <hey@rias.be>
2020-08-21 09:14:34 +0200

    Add test

--- tests/Feature/ReceiptsTest.php
+++ tests/Feature/ReceiptsTest.php
@@ -12,12 +12,16 @@ class ReceiptsTest extends FeatureTestCase
         $receipt = new Receipt([
             'amount' => '12.45',
             'tax' => '4.36',
+            'earnings' => '10.45',
+            'fee' => '2.00',
             'currency' => 'EUR',
         ]);
 
         $this->assertSame('€12.45', $receipt->amount());
         $this->assertSame('12.45', $receipt->amount);
         $this->assertSame('€4.36', $receipt->tax());
+        $this->assertSame('€10.45', $receipt->earnings());
+        $this->assertSame('€2.00', $receipt->fee());
         $this->assertInstanceOf(Currency::class, $receipt->currency());
         $this->assertSame('EUR', $receipt->currency()->getCode());
     }
