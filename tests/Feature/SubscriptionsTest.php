8da3cefc13702131de43ec5291572fe17126bffb
Dries Vints <dries@vints.io>
2020-08-28 14:11:38 +0200

    Refactor tests and update contribution guide

--- tests/Feature/SubscriptionsTest.php
+++ tests/Feature/SubscriptionsTest.php
@@ -184,7 +184,7 @@ class SubscriptionsTest extends FeatureTestCase
 
     public function test_subscriptions_can_retrieve_their_payment_info()
     {
-        if (! isset($_SERVER['PADDLE_TEST_PLAN_HOBBY'], $_SERVER['PADDLE_TEST_SUBSCRIPTION'])) {
+        if (! isset($_SERVER['CI'], $_SERVER['PADDLE_TEST_SUBSCRIPTION'])) {
             $this->markTestSkipped('Plan and/or subscription not configured.');
         }
 
@@ -193,14 +193,12 @@ class SubscriptionsTest extends FeatureTestCase
         $subscription = $billable->subscriptions()->create([
             'name' => 'main',
             'paddle_id' => $_SERVER['PADDLE_TEST_SUBSCRIPTION'],
-            'paddle_plan' => $_SERVER['PADDLE_TEST_PLAN_HOBBY'],
+            'paddle_plan' => 12345,
             'paddle_status' => Subscription::STATUS_ACTIVE,
             'quantity' => 1,
         ]);
 
         $this->assertSame('john@example.com', $subscription->paddleEmail());
         $this->assertSame('card', $subscription->paymentMethod());
-        $this->assertSame('master', $subscription->cardBrand());
-        $this->assertSame('4050', $subscription->cardLastFour());
     }
 }
