42a2bff1cf467688b722e4b01f29f7999439c1da
hoangdv <dvhoangfh1@gmail.com>
2021-07-14 18:05:28 +0700

    update environment sandbox paddle js

--- resources/views/js.blade.php
+++ resources/views/js.blade.php
@@ -2,5 +2,8 @@
 
 <script src="https://cdn.paddle.com/paddle/paddle.js"></script>
 <script type="text/javascript">
+    @if(config('cashier.sandbox'))
+        Paddle.Environment.set('sandbox');
+    @endif
     Paddle.Setup(@json($vendor));
 </script>
