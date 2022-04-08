4312864835a191bcb84eb27d5e45cfe776866ae2
Dries Vints <dries@vints.io>
2022-04-08 14:27:28 +0200

    Update Payment.php

--- src/Payment.php
+++ src/Payment.php
@@ -127,7 +127,7 @@ class Payment implements Arrayable, Jsonable, JsonSerializable
      *
      * @return array
      */
-    public function jsonSerialize()
+    public function jsonSerialize(): mixed
     {
         return $this->toArray();
     }
