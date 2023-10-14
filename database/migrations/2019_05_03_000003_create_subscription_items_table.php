db6fe08391bc81c1c66bc052a79108b1cd3bedc1
Dries Vints <dries@vints.be>
2023-10-14 13:18:21 +0200

    wip

--- database/migrations/2019_05_03_000003_create_subscription_items_table.php
+++ database/migrations/2019_05_03_000003_create_subscription_items_table.php
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('subscriptions');
+        Schema::dropIfExists('subscription_items');
     }
 };
--- src/Concerns/PerformsCharges.php
+++ src/Concerns/PerformsCharges.php
@@ -5,7 +5,6 @@ namespace Laravel\Paddle\Concerns;
 use Laravel\Paddle\Cashier;
 use Laravel\Paddle\Checkout;
 use Laravel\Paddle\Subscription;
-use LogicException;
 
 trait PerformsCharges
 {
@@ -39,38 +38,6 @@ trait PerformsCharges
         return $this->checkout($prices, 1, array_merge($custom, [
             'subscription_type' => $type,
         ]));
-    }
-
-    /**
-     * Generate a new pay link.
-     *
-     * @param  array  $payload
-     * @return string
-     */
-    protected function generatePayLink(array $payload)
-    {
-        $payload['customer_email'] = $payload['customer_email'] ?? (string) $this->paddleEmail();
-        $payload['customer_country'] = $payload['customer_country'] ?? (string) $this->paddleCountry();
-        $payload['customer_postcode'] = $payload['customer_postcode'] ?? (string) $this->paddlePostcode();
-
-        // We'll need a way to identify the user in any webhook we're catching so before
-        // we make the API request we'll attach the authentication identifier to this
-        // payload so we can match it back to a user when handling Paddle webhooks.
-        if (! isset($payload['passthrough'])) {
-            $payload['passthrough'] = [];
-        }
-
-        if (! is_array($payload['passthrough'])) {
-            throw new LogicException('The value for "passthrough" always needs to be an array.');
-        }
-
-        $payload['passthrough'] = json_encode($payload['passthrough']);
-
-        $payload = array_map(function ($value) {
-            return is_string($value) ? trim($value) : $value;
-        }, $payload);
-
-        return Cashier::post('/product/generate_pay_link', $payload)['response']['url'];
     }
 
     /**
