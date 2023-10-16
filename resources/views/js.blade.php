8f04d412af5dd17039aebfba12919a49cd96a1e7
Dries Vints <dries@vints.be>
2023-10-16 10:47:35 +0200

    Update js.blade.php

--- resources/views/js.blade.php
+++ resources/views/js.blade.php
@@ -1,11 +1,13 @@
 @php($vendor = ['vendor' => (int) config('cashier.vendor_id')])
 
 <script src="https://cdn.paddle.com/paddle/paddle.js"></script>
+
 @if (config('cashier.sandbox'))
     <script type="text/javascript">
         Paddle.Environment.set('sandbox');
     </script>
 @endif
+
 <script type="text/javascript">
     Paddle.Setup(@json($vendor));
 </script>
