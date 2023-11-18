6a922f7021475f94f9ea226418bb36d38178d9c8
Dries Vints <dries@vints.be>
2023-11-18 17:19:08 +0100

    wip

--- resources/views/js.blade.php
+++ resources/views/js.blade.php
@@ -3,6 +3,10 @@ $seller = array_filter([
     'seller' => (int) config('cashier.seller_id'),
     'pwAuth' => (int) config('cashier.retain_key'),
 ]);
+
+if (isset($seller['pwAuth']) && Auth::check() && $customer = Auth::user()->customer) {
+    $seller['pwCustomer'] = ['id' => $customer->paddle_id];
+}
 ?>
 
 <script src="https://cdn.paddle.com/paddle/v2/paddle.js"></script>
