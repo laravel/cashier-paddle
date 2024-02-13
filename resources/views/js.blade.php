7c263e042cb49d89270f8332ceebc7aa998c2fc9
Helge Sverre <helge.sverre@gmail.com>
2024-02-13 15:34:10 +0100

    Add ability to provide client_side_token instead of seller_id, the client side token will take precedence over the seller id, as you cannot specify both.

--- resources/views/js.blade.php
+++ resources/views/js.blade.php
@@ -1,8 +1,13 @@
 <?php
 $seller = array_filter([
-    'seller' => (int) config('cashier.seller_id'),
-    'pwAuth' => (int) config('cashier.retain_key'),
+    'pwAuth' => (int)config('cashier.retain_key'),
 ]);
+
+if (config('cashier.client_side_token')) {
+    $seller['token'] = config('cashier.client_side_token');
+} elseif (config('cashier.seller_id')) {
+    $seller['seller'] = (int)config('cashier.seller_id');
+}
 
 if (isset($seller['pwAuth']) && Auth::check() && $customer = Auth::user()->customer) {
     $seller['pwCustomer'] = ['id' => $customer->paddle_id];
