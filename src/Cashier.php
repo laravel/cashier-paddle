a43a24673caa226484a084e94c4dc9afffe44bbd
Dries Vints <dries@vints.be>
2024-05-21 10:59:32 +0200

    wip

--- src/Cashier.php
+++ src/Cashier.php
@@ -125,7 +125,7 @@ class Cashier
         $response = Http::withToken($apiKey)
             ->withUserAgent('Laravel\Paddle/'.static::VERSION)
             ->withHeaders(['Paddle-Version' => 1])
-            ->$method("{$host}/{$uri}", is_null($payload) && $method !== 'GET' ? '{}' : $payload);
+            ->$method("{$host}/{$uri}", $payload);
 
         if (isset($response['error'])) {
             $message = "Paddle API error '{$response['error']['detail']}' occurred";
