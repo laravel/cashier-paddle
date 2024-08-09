9043ccebc2faf2f3bd13f12d0a83c1a194ef8c91
Dries Vints <dries@vints.be>
2024-08-09 10:56:00 +0200

    Allow false values in checkout

--- src/Components/Checkout.php
+++ src/Components/Checkout.php
@@ -41,7 +41,10 @@ class Checkout extends Component
         $options['settings']['frameTarget'] = $this->id;
         $options['settings']['frameInitialHeight'] = $this->height;
 
-        $options['settings'] = array_filter(array_merge($options['settings'], $this->settings));
+        $options['settings'] = array_filter(
+            array_merge($options['settings'], $this->settings),
+            fn ($option) => ! is_null($option)
+        );
 
         return $options;
     }
