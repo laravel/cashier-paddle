d26db802388287938d24c5439d8c8988293d62ce
Mike van Egmond <mikevanegmond1998@outlook.com>
2025-05-20 23:32:53 +0200

    chore(#298): linter

--- src/CashierServiceProvider.php
+++ src/CashierServiceProvider.php
@@ -95,7 +95,7 @@ class CashierServiceProvider extends ServiceProvider
     {
         Blade::directive('paddleJS', function ($expression) {
             $expression = $expression ?: '[]';
-            return '<?php echo view("cashier::js", ["nonce" => ' . $expression . '["nonce"] ?? ""]); ?>';
+            return '<?php echo view("cashier::js", ["nonce" => '.$expression.'["nonce"] ?? ""]); ?>';
         });
     }
 
