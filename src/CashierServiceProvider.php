28116469b7013ecd575528758ffa2f1202292fba
Mike van Egmond <mikevanegmond1998@outlook.com>
2025-05-20 23:26:31 +0200

    fix(#298): correctly parse and send expression array to view

--- src/CashierServiceProvider.php
+++ src/CashierServiceProvider.php
@@ -94,7 +94,8 @@ class CashierServiceProvider extends ServiceProvider
     protected function bootDirectives()
     {
         Blade::directive('paddleJS', function ($expression) {
-            return "<?php echo view('cashier::js', ['nonce' => {$expression}['nonce'] ?? '']); ?>";
+            $expression = $expression ?: '[]';
+            return '<?php echo view("cashier::js", ["nonce" => ' . $expression . '["nonce"] ?? ""]); ?>';
         });
     }
 
